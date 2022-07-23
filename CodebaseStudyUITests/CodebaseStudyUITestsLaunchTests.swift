017387809a1641fe9c711d697eb25b36462760b0
이석호 <mercen@mercen.net>
2022-07-23 20:51:41 +0900

    Initial Commit

---
+++ CodebaseStudyUITests/CodebaseStudyUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CodebaseStudyUITestsLaunchTests.swift
+//  CodebaseStudyUITests
+//
+//  Created by Mercen on 2022/07/23.
+//
+
+import XCTest
+
+class CodebaseStudyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
